a9cbee9db652d12c07debd2a5bbc8f2ff940f7db
tennyballs <player130games@gmail.com>
2025-10-30 00:22:55 -0700

    fixed some stuff now webgl should work

--- src/Easings.h
+++ src/Easings.h
@@ -1,10 +1,30 @@
 #ifndef EASINGS_H
 #define EASINGS_H
+#include <math.h>
 
-double linear(double t);
-double instant(double t);
-double inExpo(double t);
-double outExpo(double t);
-double inOutExpo(double t);
+double linear(double t)
+{
+    return t;
+}
+
+double instant(double t)
+{
+    return 1;
+}
+
+double inExpo(double t)
+{
+    return t == 0 ? 0 : pow(2, 10 * t - 10);
+}
+
+double outExpo(double t)
+{
+    return t == 1 ? 1 : 1 - pow(2, -10 * t);
+}
+
+double inOutExpo(double t)
+{
+    return t == 0 ? 0 : t == 1 ? 1 : t < 0.5 ? pow(2, 20 * t - 10) / 2 : (2 - pow(2, -20 * t + 10)) / 2;
+}
 
 #endif // EASINGS_H
--- src/main.c
+++ src/main.c
@@ -2,8 +2,12 @@
 #include <math.h>
 #include <stdbool.h>
 
-#include <emscripten/emscripten.h>
-#include <emscripten/html5.h>
+
+#include <emscripten/html5_webgl.h>
+
+// GL
+#define GLFW_INCLUDE_ES3
+#include <GL/glfw.h>
 
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -21,35 +25,55 @@ EM_BOOL resizeCallback(int eventType, const EmscriptenUiEvent *uiEvent, void *us
     SCREEN_CENTER_X = (double) SCREEN_WIDTH / 2.0;
     SCREEN_CENTER_Y = (double) SCREEN_HEIGHT / 2.0;
 
-    emscripten_set_canvas_element_size("canvas", SCREEN_WIDTH, SCREEN_HEIGHT);
+    emscripten_set_canvas_element_size("#canvas", SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    return false;
+    // printf("%i, %i\n", SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    return true;
 }
+
+struct MainData
+{
+    EmscriptenWebGLContextAttributes attrs;
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE handle;
+} MainData;
+
+// typedef struct __attribute__((packed)) Vertex {
+//     GLuint 
+// } VertexObject;
 
 void loop()
 {
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glFlush();
 }
 
 int main()
 {
-    double width, height;
-
-    emscripten_get_element_css_size("body", &width, &height);
-
-
-    SCREEN_WIDTH = (unsigned int)width;
-    SCREEN_HEIGHT = (unsigned int)height;
-
-    SCREEN_CENTER_X = width / 2;
-    SCREEN_CENTER_Y = height / 2;
+    if (!glfwInit()) {
+        return -1;
+    }
+    emscripten_webgl_init_context_attributes(&MainData.attrs);
+    MainData.attrs.alpha = false;
+    MainData.attrs.antialias = false;
+    MainData.attrs.majorVersion = 2;
+    MainData.handle = emscripten_webgl_create_context("#canvas", &MainData.attrs);
+    if(emscripten_webgl_make_context_current(MainData.handle)){
+        return -1;
+    }
+    double WIDTH, HEIGHT;
+    emscripten_get_element_css_size("body", &WIDTH, &HEIGHT);
+    SCREEN_WIDTH = WIDTH;
+    SCREEN_HEIGHT = HEIGHT;
+    SCREEN_CENTER_X = WIDTH / 2.0;
+    SCREEN_CENTER_Y = HEIGHT / 2.0;
+    emscripten_set_canvas_element_size("#canvas", SCREEN_WIDTH, SCREEN_HEIGHT);
 
     emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, NULL, true, resizeCallback);
-    emscripten_set_canvas_element_size("canvas", SCREEN_WIDTH, SCREEN_HEIGHT);
-    
-    // stuff below here does the rest of the work.
 
+    printf("[%i, %i]\n", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    printf("Success!\n");
     emscripten_set_main_loop(loop, -1, true);
     return 0;
 }
